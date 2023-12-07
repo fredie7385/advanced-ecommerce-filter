c9803c225df69f0a3c74d82ab8ac9fb22ba852da
fredie7385 <mukii7385@gmail.com>
2023-12-07 15:12:21 +0300

    updated

--- src/Sidebar/Category/Category.jsx
+++ src/Sidebar/Category/Category.jsx
@@ -11,30 +11,10 @@ function Category({ handleChange }) {
           <input onChange={handleChange} type="radio" value="" name="test" />
           <span className="checkmark"></span>All
         </label>
-        <Input
-          handleChange={handleChange}
-          value="sneakers"
-          title="Sneakers"
-          name="test"
-        />
-        <Input
-          handleChange={handleChange}
-          value="flats"
-          title="Flats"
-          name="test"
-        />
-        <Input
-          handleChange={handleChange}
-          value="sandals"
-          title="Sandals"
-          name="test"
-        />
-        <Input
-          handleChange={handleChange}
-          value="heels"
-          title="Heels"
-          name="test"
-        />
+        <Input handleChange={handleChange} value="sneakers" title="Sneakers" name="test" />
+        <Input handleChange={handleChange} value="flats" title="Flats" name="test" />
+        <Input handleChange={handleChange} value="sandals" title="Sandals" name="test" />
+        <Input handleChange={handleChange} value="heels" title="Heels" name="test" />
       </div>
     </div>
   );
